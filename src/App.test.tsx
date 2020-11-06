5d89456991e2e028e4915b591da777dba893eec5
gollemtimes@gmail.com <gollemtimes@gmail.com>
2020-11-06 16:49:54 +0100

    test passed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/GollemTimes/i);
+  const linkElement = screen.getByText(/Welcome to GollemTimes page/i);
   expect(linkElement).toBeInTheDocument();
 });
